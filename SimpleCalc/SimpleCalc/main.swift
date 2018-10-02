53c0ea7e918880681eafb674ebabbe45612ef5b6
Kyle Wistrand <kyle.wistrand@gmail.com>
2018-10-01 22:52:42 -0700

    All tests passing

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,45 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        if args.count < 2 {
+            // print("Not valid input")
+            return 0
+        }
+        
+        switch args.last {
+        case "count":
+            return args.count - 1
+        case "avg":
+            var sum = 0
+            for num in args{
+                if Int(num) != nil {
+                    sum += Int(num)!
+                }
+            }
+            return sum / (args.count - 1)
+        case "fact":
+            // Inspiration from https://stackoverflow.com/a/46805669
+            var sum = 1
+            for i in (1...Int(args[0])!) {
+                sum *= i
+            }
+            return sum
+        default:
+            switch args[1] {
+            case "+":
+                return Int(args[0])! + Int(args[2])!
+            case "-":
+                return Int(args[0])! - Int(args[2])!
+            case "/":
+                return Int(args[0])! / Int(args[2])!
+            case "%":
+                return Int(args[0])! % Int(args[2])!
+            case "*":
+                return Int(args[0])! * Int(args[2])!
+            default:
+                return Int(args[0])!
+            }
+        }
     }
     
     public func calculate(_ arg: String) -> Int {
